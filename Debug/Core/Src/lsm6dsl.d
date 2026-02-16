279178f696c93666ba938383240cf15870e9ff2e
shaun210 <Shaun.soobagrah@mail.mcgill.ca>
2026-02-16 17:48:05 -0500

    Fix: Add .gitignore and remove build artifacts

--- Debug/Core/Src/lsm6dsl.d
+++
@@ -1,6 +0,0 @@
-Core/Src/lsm6dsl.o: ../Core/Src/lsm6dsl.c ../Core/Inc/lsm6dsl.h \
- /Users/shaunsoobagrah/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.1/Drivers/BSP/Components/lsm6dsl/../Common/accelero.h \
- /Users/shaunsoobagrah/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.1/Drivers/BSP/Components/lsm6dsl/../Common/gyro.h
-../Core/Inc/lsm6dsl.h:
-/Users/shaunsoobagrah/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.1/Drivers/BSP/Components/lsm6dsl/../Common/accelero.h:
-/Users/shaunsoobagrah/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.1/Drivers/BSP/Components/lsm6dsl/../Common/gyro.h:
